3fcc95036894fe46915fcf455b369784234c6733
chinesebear <swubear@163.com>
2026-04-18 06:20:46 +0000

    compute

---
+++ Doc/fig.compute.source.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib.font_manager as fm
+fm.fontManager.addfont('/usr/share/fonts/truetype/times.ttf')
+plt.rc('font',family='Times New Roman')
+
+datasets =["Test_60","DNA-Test_129","RNA-Test_117"]
+methods = ["DeepPPISP",  "MaSIF-site", "GraphBind","GraphPPIS", "RGN", "AGAT-PPIS", "Spatom", "RGCNPPIS", "MAPPIS"]
+indexs = ["(a)", "(b)", "(c)", "(d)", "(e)", "(f)", "(g)", "(h)", "(i)", "(j)", "(k)", "(l)", "(m)", "(n)", "(o)", "(p)", "(q)", "(r)"]
+# methods = ["D",  "M1", "G1","G2", "R1", "A", "S", "R2", "M2"]
+color =["blue", "skyblue", "royalblue", "lightgreen", "springgreen", "cyan", "deepskyblue", "orange", "red"]
+ylim = [50,500,1000,2000]
+x = np.arange(0,20,1)
+bar_width=0.4
+width=0.5
+group_width=6
+bar_width = 0.8
+title_size = 48
+lable_size = 18
+stick_size=18
+x = np.arange(0,len(datasets)*group_width,group_width)
+dx = [x, x+width, x+2*width, x+3*width, x+4*width, x+5*width, x+6*width, x+7*width, x+8*width]
+
+cpu_occ=[
+    [40.324,26.186,30.272,33.812,45.885,14.338,12.551,13.325,6.203], #Test_60
+    [36.181,23.011,27.511,29.931,40.928,12.642,11.124,12.251,5.495],  # DNA-Test_129
+    [29.169,17.709,21.564,24.159,30.813,9.904,8.596,9.449,4.261],#RNA-Test_117
+]
+
+
+RAM=[
+    [206.095,251.271,313.033,337.449,334.455,359.19,366.706,370.877,122.747],#Test_60
+    [184.642,233.316,272.775,307.849,292.526,316.439,334.607,339.749,108.874], # DNA-Test_129
+    [140.315,181.161,217.221,241.143,233.334,247.436,255.856,265.97,85.526], #RNA-Test_117
+
+]
+
+
+VRAM=[
+    [522.677,565.584,607.665,837.629,727.085,768.523,836.095,864.33,800.756], #Test_60
+    [458.243,514.105,553.088,754.455,654.27,679.148,738.931,785.155,750.069], #DNA-Test_129
+    [364.878,403.907,426.506,579.291,494.767,526.692,584.71,598.894,542.05], #RNA-Test_117
+]
+
+FLOPs=[
+    [ 542.568, 729.563, 697.331, 746.28, 723.833, 806.826, 915.552, 1534.036, 510.475], #Test_60
+    [ 505.86,636.699,642.399,674.638,655.123,742.156,834.785,1367.295,499.528], #DNA-Test_129
+    [394.568,508.28,502.424,508.071,497.053,572.268,658.814,1059.305,360.54], #RNA-Test_117
+]
+
+
+plt.rcParams.update({'font.size': 18})
+plt.subplots(4,3, figsize=(24,24))
+
+for i in range(3):
+    plt.subplot(4,3, i+1)
+    plt.bar(methods, cpu_occ[i], width=bar_width, color=color,alpha=0.5)
+    plt.ylim(0, ylim[0])
+    for j, value in enumerate(cpu_occ[i]):
+        plt.text(j-0.1,value + 5/200*ylim[0], str(round(value,1))+"%", ha='center')  # 添加文本，调整垂直位置
+    plt.ylabel('CPU Occupancy (%)', fontsize=lable_size)
+    plt.xticks(rotation=45, ha='right')
+    plt.xlabel(indexs[i]+" "+datasets[i], fontsize=20)
+    plt.xticks(fontsize=stick_size)
+    plt.yticks(fontsize=stick_size)
+
+for i in range(3):
+    plt.subplot(4,3, 3+i+1)
+    plt.bar(methods, RAM[i],  width=bar_width, color=color,alpha=0.5)
+    for j, value in enumerate(RAM[i]):
+        plt.text(j-0.1,value + 5/200*ylim[1], str(round(value)), ha='center')  # 添加文本，调整垂直位置
+    plt.ylim(0, ylim[1])
+    plt.xticks(rotation=45, ha='right')
+    plt.ylabel('RAM Usage (MB)', fontsize=lable_size)
+    plt.xlabel(indexs[i+3]+" "+datasets[i], fontsize=20)
+    plt.xticks(fontsize=stick_size)
+    plt.yticks(fontsize=stick_size)
+
+for i in range(3):
+    plt.subplot(4,3, 6+i+1)
+    plt.bar(methods, VRAM[i],  width=bar_width, color=color,alpha=0.5)
+    for j, value in enumerate(VRAM[i]):
+         plt.text(j-0.1,value + 5/200*ylim[2], str(round(value)), ha='center')  # 添加文本，调整垂直位置
+    plt.ylim(0, ylim[2])
+    plt.xticks(rotation=45, ha='right')
+    plt.ylabel('GPU VRAM Usage (MB)', fontsize=lable_size)
+    plt.xlabel(indexs[i+6]+" "+datasets[i], fontsize=20)
+    plt.yticks(fontsize=stick_size)
+    
+for i in range(3):
+    plt.subplot(4,3, 9+i+1)
+    plt.bar(methods, FLOPs[i],  width=bar_width, color=color,alpha=0.5)
+    for j, value in enumerate(FLOPs[i]):
+        plt.text(j-0.1,value + 5/200*ylim[3], str(round(value)),ha='center')  # 添加文本，调整垂直位置
+    plt.ylim(0, ylim[3])
+    plt.xticks(rotation=45, ha='right')
+    plt.ylabel('FLOPs (MFLOPs)', fontsize=lable_size)
+    plt.xlabel(indexs[i+9]+" "+datasets[i], fontsize=20)
+    plt.xticks(fontsize=stick_size)
+    plt.yticks(fontsize=stick_size)
+
+
+
+plt.tight_layout()#调整整体空白
+plt.savefig("Doc/fig/fig.compute.space.svg",dpi=600,transparent=True)
+plt.savefig("Doc/fig/fig.compute.space.jpg",dpi=600,transparent=True)
+plt.show()
---
+++ Doc/fig.compute.time.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib.font_manager as fm
+fm.fontManager.addfont('Doc/times/times.ttf')
+plt.rc('font',family='Times New Roman')
+
+datasets =["(a) Train_355-1","(b) Test_60","(c) Test_315-28","(d) BTest_31-6","(e) UTest_31-6","(f) DNA-Train_573", "(g) DNA-Test_129", "(h) RNA-Train_495", "(i) RNA-Test_117"]
+methods = ["DeepPPISP",  "MaSIF-site", "GraphBind","GraphPPIS", "RGN", "AGAT-PPIS", "Spatom", "RGCNPPIS", "MAPPIS"]
+# methods = ["D",  "M1", "G1","G2", "R1", "A", "S", "R2", "M2"]
+color =["blue", "skyblue", "royalblue", "lightgreen", "springgreen", "cyan", "deepskyblue", "orange", "red"]
+ylim = [320, 50, 200, 30, 30, 550, 100, 500, 80]
+width=0.5
+group_width=6
+bar_width = 0.8
+x = np.arange(0,len(datasets)*group_width,group_width)
+dx = [x, x+width, x+2*width, x+3*width, x+4*width, x+5*width, x+6*width, x+7*width, x+8*width]
+data=[
+    [55.14, 15.46,  63.47,11.45,10.55,120.01, 32.21, 140.02,20.25], # DeepPPISP
+    [140.85,20.85,  98.35,12.07,12.34,280.65, 58.45, 297.25,39.42], # MaSIF-site
+    [154.44,21.75,  104.58,13.02,14.24,311.45, 50.10,314.78,43.75], # GraphBind
+    [201.45,27.46, 120.45,17.51,17.08,400.25, 60.65, 328.86,46.71], # GraphPPIS
+    [170.35,25.27, 104.11,13.24,13.40,339.45, 52.85, 284.96,40.25], # RGN
+    [284.23,38.12, 143.96,21.08,21.19,512.29, 74.12, 462.14,67.42], # AGAT-PPIS
+    [211.92,30.79, 122.07,18.76,18.34,422.23, 61.47, 322.78,50.88], # Spatom
+    [199.72,29.50, 115.05,15.34,15.21,402.73, 55.22, 301.58,47.36], # RGCNPPIS
+    [110.17,17.15,  90.76, 6.93, 6.97,150.15, 37.94, 148.08,27.68],# MAPPIS
+]
+errs=[
+    [3.14, 1.46,  6.47, 0.45, 1.55, 10.01, 3.21, 8.02, 1.25], # DeepPPISP
+    [5.85, 1.85,  4.35, 0.87, 1.34, 15.65, 1.45, 5.25, 1.42], # MaSIF-site
+    [5.44, 1.75,  4.58, 1.02, 1.24, 10.45, 2.10, 8.78, 1.75], # GraphBind
+    [3.45, 1.46,  5.45, 0.51, 1.08, 9.25, 3.65, 10.86, 2.71], # GraphPPIS
+    [4.35, 1.27,  4.11, 0.24, 1.40, 9.45, 2.85, 8.96, 2.25], # RGN
+    [5.23, 2.12,  3.96, 1.08, 1.19, 10.29, 2.12, 8.14, 2.42], # AGAT-PPIS
+    [7.92, 2.79,  2.07, 0.76, 1.34, 12.23, 1.47, 8.78, 2.88], # Spatom
+    [7.72, 2.50,  5.05, 0.34, 1.21, 12.73, 1.22, 11.58, 1.36], # RGCNPPIS
+    [8.17, 2.15,  2.76, 0.93, 1.07, 12.15, 3.94, 10.08, 2.68],# MAPPIS
+]
+data = np.array(data)
+errs= np.array(errs)
+error_attri = dict(elinewidth = 1, ecolor="black", capsize = 3)
+
+plt.rcParams.update({'font.size': 18})
+plt.subplots(3,3, figsize=(15,15), dpi=600)
+for i in range(9):
+    plt.subplot(3, 3, i+1)
+    plt.bar(methods, data[:,i], width=bar_width,  yerr=errs[:,i],  error_kw=error_attri,color=color,alpha=0.5)
+    plt.xticks(rotation=45, ha='right')
+    plt.ylabel('Time (s)', fontsize=18)
+    plt.ylim(0,ylim[i])
+    plt.xlabel(datasets[i], fontsize=20)
+    # plt.title(datasets[i], fontsize=24)
+    # plt.grid()
+
+
+plt.tight_layout()#调整整体空白
+plt.savefig("Doc/fig/fig.compute.time.svg", dpi=600,transparent=True)
+plt.savefig("Doc/fig/fig.compute.time.jpg", dpi=600,transparent=True)
+plt.show()
